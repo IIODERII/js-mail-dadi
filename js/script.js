3f729c11bad64fb13dff03926ce6b94543fcb6a3
IIODERII <oderrisi@gmail.com>
2023-10-12 17:02:20 +0200

    added classes to messages in mail side

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const mailMessage = document.createElement('div');
+mailMessage.className = ('p-3') ;
+
 const mailBtn = document.querySelector("#mail-side button");
 const listMail = [
     'banana1@gmail.com',
@@ -13,13 +16,25 @@ const listMail = [
 ]
 
 mailBtn.addEventListener("click" , function(){
+    mailMessage.classList.add('d-none');
     const userMail = document.getElementById('mail').value;
     // console.log(userMail);
 
     let trovata = false;
     for(let i = 0 ; i < listMail.length ; i++){
         const currentMail = listMail[i]
-        console.log(currentMail);
+        mailMessage.classList.remove('d-none');
+        // console.log(currentMail);
+        if(currentMail === userMail){
+            trovata = true ;
+        }
     }
-    // console.log(trovata);
+    if(trovata){
+        mailMessage.classList.add('bg-success');
+        mailMessage.innerHTML = '&check; La tua mail è stata trovata: ' + userMail;
+    }else{
+        mailMessage.classList.add('bg-danger');
+        mailMessage.innerHTML = '&cross; La tua mail non è stata trovata: ' + userMail;
+    }
+    document.querySelector('.messages').append(mailMessage);
 })
