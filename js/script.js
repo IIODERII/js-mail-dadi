a79d12f8098c9c16b7f08828266e42f4fcdf58dd
IIODERII <oderrisi@gmail.com>
2023-10-12 16:12:53 +0200

    added event in js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const mailBtn = document.querySelector("#mail-side .btn");
+const listMail = [
+    'banana1@gmail.com',
+    'fragola2@hotmail.com',
+    'kiwi2@libero.com',
+    'mela4@aruba.com',
+    'pesca5@gmail.com',
+    'anguria6@hotmail.com',
+    'pera7@hotmail.com',
+    'albicocca8@hotmail.com',
+    'melone9@hotmail.com',
+    'mango10@hotmail.com',
+]
+
+mailBtn.addEventListener('click' , function(){
+    const userMail = document.getElementById('mail').value;
+    // console.log(userMail);
+    
+})
